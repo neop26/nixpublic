23db6eba5d92d0277e3bc05c5dec10e92ccb5167
Aben Samuel <aben26@gmail.com>
2023-10-28 05:51:32 +1300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,140 +1,171 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
+
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
+  
+  # Enable experimental features
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"];
+    auto-optimise-store = true;
+  };
 
-  # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  # Enable Grub bootloader and filesystems  
+  boot = {
+    supportedFilesystems = [ "ntfs" "exfat" "mtpfs" ];
+    loader = {
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+      grub = {
+        enable = true;
+        devices = [ "nodev" ];
+        efiSupport = true;
+        useOSProber = true;
+        extraEntries = ''
+        menuentry "Reboot" {
+          reboot
+        }
+        menuentry "Poweroff" {
+          halt
+        }
+'';
+      };
+    };
+  };
 
-  networking.hostName = "nixos-neo"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking = {
+    hostName = "mutant";
+    networkmanager.enable = true;
+  };
+
+  # Set your time zone.
+  time.timeZone = "Africa/Nairobi";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-
- # Enable Static DNS
-  networking.nameservers = [ "192.168.1.11" ];
-
-# Reduce Swap
-boot.kernel.sysctl = { "vm.swappiness" = 10;};
-
-  # Enable Bluetooth
-  
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-
-  # Set your time zone.
-  time.timeZone = "Pacific/Auckland";
-
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_NZ.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_NZ.UTF-8";
-    LC_IDENTIFICATION = "en_NZ.UTF-8";
-    LC_MEASUREMENT = "en_NZ.UTF-8";
-    LC_MONETARY = "en_NZ.UTF-8";
-    LC_NAME = "en_NZ.UTF-8";
-    LC_NUMERIC = "en_NZ.UTF-8";
-    LC_PAPER = "en_NZ.UTF-8";
-    LC_TELEPHONE = "en_NZ.UTF-8";
-    LC_TIME = "en_NZ.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  #   useXkbConfig = true; # use xkbOptions in tty.
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+ services.xserver = {
+   enable = true;
+   displayManager = {
+     sddm.enable = true;
+     sddm.theme = "${import ./sddm-theme.nix { inherit pkgs; }}";
+   };
+ };
+  
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
+  # Enable hyprland
+  programs.hyprland = {
+    enable = true;
   };
+  programs.hyprland.xwayland = {
+    hidpi = true;
+    enable = true;
+  };
+  
+  environment.sessionVariables = {
+    # If cursor becomes invisible
+    # WLR_NO_HARDWARE_CURSORS = "1";
+    # Hint electron apps to use wayland
+    # NIXOS_OZONE_WL = "1";
+  };
+  
+  hardware = {
+    opengl.enable = true;
+    bluetooth.enable = true;
+  }; 
+  services.blueman.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+
 
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services.printing = {
+    enable = true;
+    
+  };
 
-  # Enable sound with pipewire.
-  sound.enable = true;
+  # Enable sound with pipewire
   hardware.pulseaudio.enable = false;
+  sound.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true; 
+  };  
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+  virtualisation.libvirtd.enable = true;
+  programs.dconf.enable = true;
+  
+  programs.thunar.enable = true;
+  programs.thunar.plugins = with pkgs.xfce; [
+   thunar-archive-plugin
+   thunar-volman
+  ];
+
+  services.gvfs.enable = true; # Mount, trash, and other functionalities
+  services.tumbler.enable = true; # Thumbnail support for images
+  
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.neo = {
+  users.users.nx0enjoyer = {
     isNormalUser = true;
-    description = "neo";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "wheel" "libvirtd" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
-      firefox
-      kate
-    #  thunderbird
+      # packages go here
     ];
   };
-
-  # Allow unfree packages
+  
+  fonts = {
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        monospace = [ "Meslo LG M Regular Nerd Font Complete Mono" ];
+      };
+    };
+    fonts = with pkgs; [ (nerdfonts.override { fonts = [ "Meslo" "JetBrainsMono" ]; }) ];
+  };
+  
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowInsecure = true;
+  nixpkgs.config.PermittedInsecurePackages = [
+
+  ]; 
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-      putty
-      microsoft-edge
-      dig
-      twingate
-      spotify
-      neofetch
-      remmina
-      git
-      fish
-      termius
-      virt-manager
-      ranger
-      ];
-
-# Automatic Garbage Collection
-nix.gc = {
-                automatic = true;
-                dates = "weekly";
-                options = "--delete-older-than 7d";
-        };
-
-# Auto system update
-system.autoUpgrade = {
-      enable = true;
-};
+    vim
+    gcc
+    gnumake
+    pamixer
+    libsForQt5.qt5.qtquickcontrols2
+    libsForQt5.qt5.qtgraphicaleffects
+    home-manager
+  ];  
+  
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
 
   # Some programs need SUID wrappers, can be configured further or are
-
   # started in user sessions.
   # programs.mtr.enable = true;
   # programs.gnupg.agent = {
@@ -143,22 +174,26 @@ system.autoUpgrade = {
   # };
 
   # List services that you want to enable:
-
+   
+  # Enable tlp
+  services.tlp.enable = true;
+  
   # Enable the OpenSSH daemon.
-   services.openssh.enable = true;
-  # Enable Twingate
-  services.twingate.enable = true;
-  # Enable Flatpak Service
-   services.flatpak.enable = true;
+  services.openssh.enable = true;
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # on your system were taken. It's perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
