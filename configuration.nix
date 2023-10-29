1127544f28575a0d8879a17f05c544286e5bd8e7
neo-nix-pl <aben26@gmail.com>
2023-10-29 23:00:32 +1300

    Cleaned File and added few more apps

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,8 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos-neo"; # Define your hostname.
+  # Hostname for the System
+     networking.hostName = "nixos-neo";
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -22,92 +23,88 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+     networking.networkmanager.enable = true;
 
  # Enable Static DNS
-  networking.nameservers = [ "192.168.1.11" ];
+     networking.nameservers = [ "192.168.1.11" ];
 
-# Reduce Swap
-boot.kernel.sysctl = { "vm.swappiness" = 10;};
+ # Reduce Swap
+     boot.kernel.sysctl = { "vm.swappiness" = 10;};
 
-  # Enable Bluetooth
-  
-  hardware.bluetooth.enable = true;
- # hardware.bluetooth.powerOnBoot = true;
-  services.blueman.enable = true;
+ # Enable Bluetooth
+      hardware.bluetooth.enable = true;
+ # Hardware.bluetooth.powerOnBoot = true;
+      services.blueman.enable = true;
 
   # Set your time zone.
-  time.timeZone = "Pacific/Auckland";
+     time.timeZone = "Pacific/Auckland";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_NZ.UTF-8";
+     i18n.defaultLocale = "en_NZ.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_NZ.UTF-8";
-    LC_IDENTIFICATION = "en_NZ.UTF-8";
-    LC_MEASUREMENT = "en_NZ.UTF-8";
-    LC_MONETARY = "en_NZ.UTF-8";
-    LC_NAME = "en_NZ.UTF-8";
-    LC_NUMERIC = "en_NZ.UTF-8";
-    LC_PAPER = "en_NZ.UTF-8";
-    LC_TELEPHONE = "en_NZ.UTF-8";
-    LC_TIME = "en_NZ.UTF-8";
-  };
+     i18n.extraLocaleSettings = {
+     LC_ADDRESS = "en_NZ.UTF-8";
+     LC_IDENTIFICATION = "en_NZ.UTF-8";
+     LC_MEASUREMENT = "en_NZ.UTF-8";
+     LC_MONETARY = "en_NZ.UTF-8";
+     LC_NAME = "en_NZ.UTF-8";
+     LC_NUMERIC = "en_NZ.UTF-8";
+     LC_PAPER = "en_NZ.UTF-8";
+     LC_TELEPHONE = "en_NZ.UTF-8";
+     LC_TIME = "en_NZ.UTF-8";
+     };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+     services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+     services.xserver.displayManager.sddm.enable = true;
+     services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
+     services.xserver = {
+      layout = "us";
+      xkbVariant = "";
+     };
 
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+     services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+     sound.enable = true;
+     hardware.pulseaudio.enable = false;
+     security.rtkit.enable = true;
+     services.pipewire = {
+     enable = true;
+     alsa.enable = true;
+     alsa.support32Bit = true;
+     pulse.enable = true;
+     # If you want to use JACK applications, uncomment this
+     #jack.enable = true;
+     # use the example session manager (no others are packaged yet so this is enabled by default,
+     # no need to redefine it in your config for now)
+     #media-session.enable = true;
+     };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+     # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.neo = {
-    isNormalUser = true;
-    description = "neo";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      firefox
+     users.users.neo = {
+     isNormalUser = true;
+     description = "neo";
+     extraGroups = [ "networkmanager" "wheel" ];
+     packages = with pkgs; [
+     firefox
       kate
-    #  thunderbird
-    ];
-  };
+     ];
+     };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
+ # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+ # List packages installed in system profile. To search, run:
+   # $ nix search wget
+   environment.systemPackages = with pkgs; [
       putty
       microsoft-edge
       dig
@@ -121,21 +118,28 @@ boot.kernel.sysctl = { "vm.swappiness" = 10;};
       virt-manager
       ranger
       kitty
+      wget
+      ncdu # disk manager
+      gparted
+      htop
+      azure-cli
+      freerdp
+      unzip
       ];
 
 # Automatic Garbage Collection
-nix.gc = {
+        nix.gc = {
                 automatic = true;
                 dates = "weekly";
                 options = "--delete-older-than 7d";
         };
 
 # Auto system update
-system.autoUpgrade = {
-      enable = true;
-};
+       system.autoUpgrade = {
+        enable = true;
+       };
 
-  # Some programs need SUID wrappers, can be configured further or are
+# Some programs need SUID wrappers, can be configured further or are
 
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -144,13 +148,12 @@ system.autoUpgrade = {
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-   services.openssh.enable = true;
-  # Enable Twingate
-  services.twingate.enable = true;
-  # Enable Flatpak Service
+ # List services that you want to enable:
+ # Enable the OpenSSH daemon.
+    services.openssh.enable = true;
+ # Enable Twingate
+    services.twingate.enable = true;
+ # Enable Flatpak Service
    services.flatpak.enable = true;
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -164,6 +167,6 @@ system.autoUpgrade = {
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+    system.stateVersion = "23.05"; # Did you read the comment?
 
 }
